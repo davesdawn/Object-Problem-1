9da2cde67943bbabf508d457e9c97083fdf9d95a
unknown <daveasmah@yahoo.com>
2022-07-27 21:14:55 -0400

    solution commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /* Create a sample object, then write a JavaScript function that
 deletes the property of an object passed to it.*/
 
+
+
+
+
+
 // Create an object
-
-// Write the function the deletes the property of a specified object
-
-//Function takes 2 parameters
-
 let spaceship = {
     homePlanet: 'Earth',
     color: 'silver',
@@ -15,10 +15,17 @@ let spaceship = {
     flightPath: ['Venus', 'Mars', 'Saturn']
 };
 
+// Write the function the deletes the property of a specified object
+//Function takes 2 parameters
 function removePty(object, property) {
-    delete object[property]; 
+    delete object[property]; //delete spaceship['Fuel Type'];
     return object;
 
 }
 removePty(spaceship, 'Fuel Type');
-console.log(spaceship);
+console.log(spaceship); /* prints  {
+                        color: "silver",
+                        flightPath: ["Venus", "Mars", "Saturn"],
+                        homePlanet: "Earth",
+                        numCrew: 5
+                        }*/
